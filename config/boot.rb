25eb8fc79673bc60e3176dc7ab2ebc7a2097da80
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2011-08-04 22:48:19 -0400

    Cleaned up a bit. Bump to 0.1.0b.

--- config/boot.rb
+++ config/boot.rb
@@ -1,12 +1,14 @@
 # Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
-MARVIN_VERSION = "0.1.0 alpha beta"
+MARVIN_VERSION = "0.1.0 beta"
 
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
+
 Bundler.require(:default, PADRINO_ENV)
+
 require 'yaml'
 require 'ostruct'
 
@@ -24,24 +26,12 @@ Blog = OpenStruct.new(
   :url => @@yaml["url"],
   :feed => feed
 )
+
 ##
 # Enable devel logging
 #
 # Padrino::Logger::Config[:development] = { :log_level => :devel, :stream => :stdout }
 # Padrino::Logger.log_static = true
 #
-
-##
-# Add your before load hooks here
-#
-Padrino.before_load do
-
-end
-
-##
-# Add your after load hooks here
-#
-Padrino.after_load do
-end
 
 Padrino.load!
